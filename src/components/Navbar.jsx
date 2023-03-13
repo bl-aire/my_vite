e5d9d8d19841f031a1c8efb0c9609721824205de
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-13 23:05:52 +0100

    Style nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,15 @@
+import logo from '../assets/react.svg'
+
 export default function Navbar() {
     return(
-        <h1>Navbar!</h1>
+        <nav className="nav">
+            <img src={logo} width="70px" />
+            <h1 className='facts'>BeeFacts</h1>
+            <ul className="nav-items">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contract</li>
+            </ul>
+        </nav>
     )
 }
